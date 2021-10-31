d084b3449786154dc7083ab2541758f105a77864
Imaad Uddin <imaaduddin27@gmail.com>
2021-10-31 02:13:56 -0500

    reverse integer solution

--- Reverse-Integer.py
+++ Reverse-Integer.py
@@ -48,3 +48,24 @@ class Solution:
         if not -2147483648 < sum < 2147483647:
             return 0
         return sign * sum
+    
+
+# Solution from: https://youtu.be/HAgLH58IgJQ
+import math
+class Solution:
+    def reverse(self, x):
+        MIN = -2147483648
+        MAX = 2147483648
+        
+        res = 0
+        while x:
+            digit = int(math.fmod(x, 10))
+            x = int(x / 10)
+
+            if (res > MAX // 10 or (res == MAX // 10 and digit >= MAX % 10)):
+                return 0
+            if (res < MIN // 10 or (res == MIN // 10 and digit <= MIN % 10)):
+                return 0
+            res = (res * 10) + digit
+        
+        return res
