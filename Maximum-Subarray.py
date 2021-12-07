b49b5f27241e5179f9889adcfaf9068136c460cd
Imaad Uddin <imaaduddin27@gmail.com>
2021-12-07 01:31:20 -0600

    maximum subarray

---
+++ Maximum-Subarray.py
@@ -0,0 +1,14 @@
+# Question Difficulty: Easy
+# Question URL: https://leetcode.com/problems/maximum-subarray/
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sub = nums[0]
+        current_sum = 0
+        
+        for i in nums:
+            if current_sum < 0:
+                current_sum = 0
+            current_sum += i
+            max_sub = max(max_sub, current_sum)
+        return max_sub
