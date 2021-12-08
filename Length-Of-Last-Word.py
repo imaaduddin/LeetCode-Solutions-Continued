6d9ebc05833352adfc479c37d870e15bd4f49d94
Imaad Uddin <imaaduddin27@gmail.com>
2021-12-07 18:42:59 -0600

    lenght of last word question

---
+++ Length-Of-Last-Word.py
@@ -0,0 +1,18 @@
+# Question Difficulty: Easy
+# Question URL: https://leetcode.com/problems/length-of-last-word/
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        # i, length = len(s) - 1, 0
+
+        # while s[i] == " ":
+        #     i -= 1
+        # while i >= 0 and s[i] != " ":
+        #     length += 1
+        #     i -= 1
+        # return length
+        
+        # Another Solution (String split version):
+        
+        
+
