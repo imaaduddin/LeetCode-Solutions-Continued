cc06aad7f7523dc4e6afd38ed2c76eff4524d2a0
Imaad Uddin <imaaduddin27@gmail.com>
2021-12-09 18:11:52 -0600

    code cleanup

--- Length-Of-Last-Word.py
+++ Length-Of-Last-Word.py
@@ -1,30 +1,45 @@
 # Question Difficulty: Easy
 # Question URL: https://leetcode.com/problems/length-of-last-word/
 
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        # i, length = len(s) - 1, 0
+# class Solution:
+#     def lengthOfLastWord(self, s: str) -> int:
+#         i, length = len(s) - 1, 0
 
-        # while s[i] == " ":
-        #     i -= 1
-        # while i >= 0 and s[i] != " ":
-        #     length += 1
-        #     i -= 1
-        # return length
+#         while s[i] == " ":
+#             i -= 1
+#         while i >= 0 and s[i] != " ":
+#             length += 1
+#             i -= 1
+#         return length
         
-        # Another Solution (String split version):
-        if s.split():
-            return len(s.split()[-1])
-        return 0
+        
+# class Solution:
+#     def lengthOfLastWord(self, s: str) -> int:   
+#         Another Solution (String split version):
+#         if s.split():
+#             return len(s.split()[-1])
+#         return 0     
+        
+        
+        
+
+
+# class Solution:
+#     def lengthOfLastWord(self, s: str) -> int:
+        
+#         Another Solution (String split version):
+#         if s.split():
+#             return len(s.split()[-1])
+#         return 0
     
-        # Another Method: Manual string length count
-        count = 0
-        for letter in s[::-1]:
-            if letter == " ":
-                if count >= 1:
-                    return count
-            else:
-                count += 1
+#         Another Method: Manual string length count
+#         count = 0
+#         for letter in s[::-1]:
+#             if letter == " ":
+#                 if count >= 1:
+#                     return count
+#             else:
+#                 count += 1
 
             
         
--- Plus-One.py
+++ Plus-One.py
@@ -1,10 +1,20 @@
 # Question Difficulty: Easy
 # Question URL: https://leetcode.com/problems/plus-one/
 
+# class Solution:
+#     def plusOne(self, digits: List[int]) -> List[int]:
+#         My attempt
+#         for number in range(len(digits)):
+#             if digits[number -1] >= 9:
+#                 return digits.append(1, 0)
+#             end = digits[number - 1] + 1
+#             return end
+
+
+        
+        
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        for number in range(len(digits)):
-            if digits[number -1] >= 9:
-                return digits.append(1, 0)
-            end = digits[number - 1] + 1
-            return end
+        
+        
+        
