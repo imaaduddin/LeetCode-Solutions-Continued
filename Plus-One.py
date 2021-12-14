35fe7edd5b625686d2d866b5b7b6a8c68c36bf36
Imaad Uddin <imaaduddin27@gmail.com>
2021-12-14 00:59:44 -0600

    some solutions

--- Plus-One.py
+++ Plus-One.py
@@ -12,9 +12,30 @@
 
 
         
+
         
+# class Solution:
+#     def plusOne(self, digits: List[int]) -> List[int]:
+#         digits = digits[::-1]
+#         one, i = 1, 0
+
+#         while one == 1:
+#             if i < len(digits):
+#                 if digits[i] == 9:
+#                     digits[i] = 0
+#                 else:
+#                     digits[i] += 1
+#                     one = 0
+#             else:
+#                 digits.append(1)
+#                 one = 0
+#             i += 1
+#         return digits[::-1]
+        
+        
+
+# Another Solution: 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        
-        
-        
+        # Method 1: Type conversion
+        return [int(x) for x in str(int("".join([str(i) for i in digits])) + 1)]
