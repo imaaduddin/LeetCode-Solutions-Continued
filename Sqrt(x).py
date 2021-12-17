8588688ecd7d3bef1f4ad6a91c8a927c7d16fd33
Imaad Uddin <imaaduddin27@gmail.com>
2021-12-16 18:10:28 -0600

    what i have so far

---
+++ Sqrt(x).py
@@ -0,0 +1,10 @@
+# Question Difficulty: Easy
+# Question URL: https://leetcode.com/problems/sqrtx/
+import math
+# My Solution:
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        square_root = math.sqrt(x)
+        
+        
+
