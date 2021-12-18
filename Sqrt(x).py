cad1b05b42e30046fc648e0448d1a138209501f3
Imaad Uddin <imaaduddin27@gmail.com>
2021-12-17 18:14:16 -0600

    other solutions

--- Sqrt(x).py
+++ Sqrt(x).py
@@ -1,12 +1,25 @@
 # Question Difficulty: Easy
 # Question URL: https://leetcode.com/problems/sqrtx/
-import math
+# import math
 # My Solution:
-class Solution:
-    def mySqrt(self, x: int) -> int:
-        square_root = math.sqrt(x)
-        rounded = math.floor(square_root)
-        return rounded
-        
+# class Solution:
+#     def mySqrt(self, x: int) -> int:
+#         square_root = math.sqrt(x)
+#         rounded = math.floor(square_root)
+#         return rounded
         
 
+
+
+
+# Another Solution:
+# class Solution:
+#     def mySqrt(self, x: int) -> int:
+#         import math
+#         return int(math.sqrt(x))
+        
+
+# Babylonian Method (More Interesting Approach):
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        
