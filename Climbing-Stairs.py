ac28a8b03194e38d220f065b37c6e3ef971c4371
Imaad Uddin <imaaduddin27@gmail.com>
2021-12-21 22:03:40 -0600

    new solution for climbin stairs

--- Climbing-Stairs.py
+++ Climbing-Stairs.py
@@ -2,10 +2,22 @@
 # Question URL: https://leetcode.com/problems/climbing-stairs/
 
 # Solution 1
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         path = {1: 1, 2: 2, 3: 3}
+
+#         for x in range(4, n+1):
+#             path[x] = path[x-1] + path[x-2]
+#         return path[n]
+
+
+# Another Solution: 
 class Solution:
     def climbStairs(self, n: int) -> int:
-        path = {1: 1, 2: 2, 3: 3}
-
-        for x in range(4, n+1):
-            path[x] = path[x-1] + path[x-2]
-        return path[n]
+        one, two = 1, 1
+        
+        for i in range(n - 1):
+            temp = one
+            one = one + two
+            two = temp
+        return one
---
+++ Remove-Duplicates-From-Sorted-List.py
@@ -0,0 +1,6 @@
+# Question Difficulty: Easy
+# Question URL: https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
