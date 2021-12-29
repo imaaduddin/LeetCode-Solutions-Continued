016366ebb6b7e06fd56f10478538d80dcc4a9011
Imaad Uddin <imaaduddin27@gmail.com>
2021-12-29 17:28:04 -0600

    solution recursive

---
+++ Maximum-Depth-Of-Binary-Tree.py
@@ -0,0 +1,9 @@
+# Question Difficulty: Easy
+# Question URL: https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+# Recursive Solution
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return (1 + max(self.maxDepth(root.left), self.maxDepth(root.right)))
