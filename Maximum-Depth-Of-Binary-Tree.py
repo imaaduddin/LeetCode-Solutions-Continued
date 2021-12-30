42805f11efa31b550bd16ac2ae3293396727080a
Imaad Uddin <imaaduddin27@gmail.com>
2021-12-30 17:27:53 -0600

    other solutions for this question

--- Maximum-Depth-Of-Binary-Tree.py
+++ Maximum-Depth-Of-Binary-Tree.py
@@ -11,5 +11,21 @@
     
 
 # Another Recursive Solution 
+# class Solution:
+#     def maxDepth(self, root: Optional[TreeNode]) -> int:
+#         if not root:
+#             return 0
+#         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+
+# Iterative BFS
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        level = 1
+        q = deque([root])
+        while q:
+            for i in range(len(q)):
+                q.popleft()
