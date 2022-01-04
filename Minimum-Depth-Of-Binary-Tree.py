f389cc5c7e1494a0ccce4286562db0e544e45911
Imaad Uddin <imaaduddin27@gmail.com>
2022-01-04 17:13:34 -0600

    DFS for question 111

--- Minimum-Depth-Of-Binary-Tree.py
+++ Minimum-Depth-Of-Binary-Tree.py
@@ -24,19 +24,36 @@
 
 
 # BFS
+# class Solution:
+#     def minDepth(self, root: Optional[TreeNode]) -> int:
+#         if not root:
+#             return 0
+        
+#         q = deque([(root, 1)])
+
+#         while q:
+#             cand, num = q.popleft()
+#             if not cand.left and not cand.right:
+#                 return num
+#             if cand.left:
+#                 q.append((cand.left, num + 1))
+#             if cand.right:
+#                 q.append((cand.right, num + 1))
+#         return -1
+
+# DFS
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        
-        q = deque([(root, 1)])
 
-        while q:
-            cand, num = q.popleft()
-            if not cand.left and not cand.right:
-                return num
-            if cand.left:
-                q.append((cand.left, num + 1))
-            if cand.right:
-                q.append((cand.right, num + 1))
-        return -1
+        if not root.left and not root.right:
+            return 1
+        
+        if not root.right and root.left:
+            return 1 + self.minDepth(root.left)
+
+        if not root.left and root.right:
+            return 1 + self.minDepth(root.right)
+        
+        return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
