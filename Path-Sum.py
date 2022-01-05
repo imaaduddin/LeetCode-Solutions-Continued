32501811ca8aae09a7eea1ebf6c78d8c107b2ef0
Imaad Uddin <imaaduddin27@gmail.com>
2022-01-05 17:01:19 -0600

    solution for LC 118

--- Path-Sum.py
+++ Path-Sum.py
@@ -2,7 +2,7 @@
 # Question URL: https://leetcode.com/problems/path-sum/
 # Question Number: 112
 
-# ALmost always in Tree problems recursion should be used
+# Almost always in Tree problems recursion should be used
 
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
