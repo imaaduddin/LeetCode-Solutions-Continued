d3825496f030fc8e299f21344459b5ef81dcbe64
Imaad Uddin <imaaduddin27@gmail.com>
2022-01-05 16:13:49 -0600

    solution to LC 112

--- Path-Sum.py
+++ Path-Sum.py
@@ -6,9 +6,16 @@
 
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        if root == None:
-            return False
-        
+        def dfs(node, curSum):
+            if not node:
+                return False 
+            
+            curSum += node.val
+            if not node.left and not node.right:
+                return curSum == targetSum
+            
+            return dfs(node.left, curSum) or dfs(node.right, curSum)
+        return dfs(root, 0)
         
         
         
