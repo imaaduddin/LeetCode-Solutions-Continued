59a5c0cb6ee246e9ca7d992ead4d6953b02777a1
Imaad Uddin <imaaduddin27@gmail.com>
2022-01-19 18:04:05 -0600

    another solution

--- Valid-Palindrome.py
+++ Valid-Palindrome.py
@@ -16,6 +16,20 @@
 
 
 # NeetCode SOlution:
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         new_str = ""
+        
+#         for c in s:
+#             if c.isalnum():
+#                 new_str += c.lower()
+        return new_str == new_str[::-1]
+    
+
+# NeetCode Solution 2:
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         
+        def alphaNum(self, c):
+            return (ord("A") <= ord(c) <= ord("Z") or ord("a") <= ord(c) <= ord("z") or ord("0") <= ord(c) <= ord("9")) 
+    
