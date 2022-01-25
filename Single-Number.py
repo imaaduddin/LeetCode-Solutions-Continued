c4845a7f1abda2ec24072545da3fa7cb90e344b6
Imaad Uddin <imaaduddin27@gmail.com>
2022-01-24 18:16:55 -0600

    solution to LC 136

--- Single-Number.py
+++ Single-Number.py
@@ -3,10 +3,26 @@
 # Question URL: https://leetcode.com/problems/single-number/
 
 # My Attempt:
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        for n in range(len(nums)):
-            if nums[n].count() > 1:
-                return False
-            else:
-                return nums
+# class Solution:
+#     def singleNumber(self, nums: List[int]) -> int:
+#         new_num = str(nums)
+#         for n in range(len(new_num)):
+#             if new_num[n].count() > 1:
+#                 return False
+#             else:
+#                 return new_num
+
+
+# DataDaft Solution:
+# class Solution:
+#     def singleNumber(self, nums: List[int]) -> int:
+#         counts = {}
+#         for n in nums:
+#             if n not in counts:
+#                 counts[n] = 1
+#             else:
+#                 del counts[n]
+#         return list(counts.keys())[0]
+    
+
+# NeetCode Solution:
