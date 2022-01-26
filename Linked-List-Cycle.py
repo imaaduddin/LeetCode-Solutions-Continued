49c65caf4c4580371f2459041cb9c02cfe1a792a
Imaad Uddin <imaaduddin27@gmail.com>
2022-01-25 18:03:53 -0600

    starter code

---
+++ Linked-List-Cycle.py
@@ -0,0 +1,7 @@
+# Question Difficulty: Easy
+# Question Number: 141
+# Question URL: https://leetcode.com/problems/linked-list-cycle/
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        
