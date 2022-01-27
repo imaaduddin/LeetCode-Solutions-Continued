10cae17558318b3f3454fdbbc1c95c23b6d36eb1
Imaad Uddin <imaaduddin27@gmail.com>
2022-01-26 19:01:33 -0600

    solution to LC 141

--- Linked-List-Cycle.py
+++ Linked-List-Cycle.py
@@ -2,6 +2,15 @@
 # Question Number: 141
 # Question URL: https://leetcode.com/problems/linked-list-cycle/
 
+# NeetCode
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
         
