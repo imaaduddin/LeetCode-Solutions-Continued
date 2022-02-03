5570521efa050c5deb6bd0d60dba266a91c86910
Imaad Uddin <imaaduddin27@gmail.com>
2022-02-02 18:05:09 -0600

    recurisve and iterative solutions

--- Binary-Tree-Preorder-Traversal.py
+++ Binary-Tree-Preorder-Traversal.py
@@ -8,8 +8,27 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# Recursive Solution: 
+# class Solution:
+#     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+#         if root is None:
+#             return []
+#         return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+
+# Iterative Solution:
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         if root is None:
             return []
-        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+        
+        stack = [root]
+        result = []
+        while stack != []:
+            root = stack.pop()
+            result.append(root.val)
+            if root.right is not None:
+                stack.append(root.right)
+            if root.left is not None:
+                stack.append(root.left)
+        return result
