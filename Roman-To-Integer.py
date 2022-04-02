acac65c9ff5002e14cde2fad24ff3804e04a9838
Imaad Uddin <imaaduddin27@gmail.com>
2022-04-02 17:41:55 -0500

    another solution to LC 13

--- Roman-To-Integer.py
+++ Roman-To-Integer.py
@@ -2,26 +2,54 @@
 
 # Difficulty is easy
 # Solution 1
+# class Solution:
+#   def romanToInt(self, s: str) -> int:
+#     roman_table = {
+#         "I": 1,
+#         "V": 5,
+#         "X": 10,
+#         "L": 50,
+#         "C": 100,
+#         "D": 500,
+#         "M": 1000
+#     }
+#     # Reverse iteration solution 
+    
+#     num = 0
+#     last = "I"
+    
+#     for numeral in s[::-1]:
+#       if roman_table[numeral] < roman_table[last]:
+#         num -= roman_table[numeral]
+#       else:
+#         num += roman_table[numeral]
+#       last = numeral
+#     return num
+
+
+# Another Solution (NeetCode)
 class Solution:
-  def romanToInt(self, s: str) -> int:
-    roman_table = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
-    # Reverse iteration solution 
-    
-    num = 0
-    last = "I"
-    
-    for numeral in s[::-1]:
-      if roman_table[numeral] < roman_table[last]:
-        num -= roman_table[numeral]
-      else:
-        num += roman_table[numeral]
-      last = numeral
-    return num
+    def romanToInt(self, s: str) -> int:
+          # largest to smallest: add them up
+          # smaller before larger: subtract smaller 
+          
+          roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+          }
+          
+          res = 0
+        
+          for i in range(len(s)):
+              if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+                res -= roman [s[i]]
+              else:
+                    res += roman[s[i]]
+          return res
+              
+      
